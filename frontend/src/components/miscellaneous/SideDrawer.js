89ee8c24d12b5d560c679eade7ea7461ce53654f
Naisha <86354320+naishara21@users.noreply.github.com>
2024-03-30 22:17:14 +0530

    Update SideDrawer.js

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -139,9 +139,9 @@ function SideDrawer() {
           CulturaLang
         </Text>
         {/* Add Home, Information, and Contact links */}
-        <Link href="/Home.htm" ml={4} color="white">Home</Link>
-        <Link href="/Information.htm" ml={4} color="white">Information</Link>
-        <Link href="/chat.htm" ml={4} color="white">Chat</Link>
+        <Link href="/Home.html" ml={4} color="white">Home</Link>
+        <Link href="/Information.html" ml={4} color="white">Information</Link>
+        <Link href="/frontend\src\App.js" ml={4} color="white">Chat</Link>
         <Link href="/Contact.htm" ml={4} color="white">Contact</Link>
         <Button variant="ghost" onClick={onOpen}>
           <i className="fas fa-search"></i>
