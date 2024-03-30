9c278d1f7c696bd3efdfe4352a34591c28d6d823
Naisha <86354320+naishara21@users.noreply.github.com>
2024-03-30 22:25:44 +0530

    Update SideDrawer.js

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -16,41 +16,24 @@ import {
   DrawerHeader,
   DrawerOverlay,
 } from "@chakra-ui/modal";
-import { Tooltip } from "@chakra-ui/tooltip";
-import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import { Avatar } from "@chakra-ui/avatar";
-import { Link } from "@chakra-ui/react"; // Import Link component from Chakra UI
+import { Link } from "@chakra-ui/react";
 import { useHistory } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 import { useToast } from "@chakra-ui/toast";
-import ChatLoading from "../ChatLoading";
 import { Spinner } from "@chakra-ui/spinner";
-import ProfileModal from "./ProfileModal";
-import NotificationBadge from "react-notification-badge";
-import { Effect } from "react-notification-badge";
-import { getSender } from "../../config/ChatLogics";
 import UserListItem from "../userAvatar/UserListItem";
-import { ChatState } from "../../Context/ChatProvider";
 
 function SideDrawer() {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [loadingChat, setLoadingChat] = useState(false);
 
-  const {
-    setSelectedChat,
-    user,
-    notification,
-    setNotification,
-    chats,
-    setChats,
-  } = ChatState();
-
-  const toast = useToast();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const history = useHistory();
+  const toast = useToast();
 
   const logoutHandler = () => {
     localStorage.removeItem("userInfo");
@@ -94,35 +77,6 @@ function SideDrawer() {
     }
   };
 
-  const accessChat = async (userId) => {
-    console.log(userId);
-
-    try {
-      setLoadingChat(true);
-      const config = {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: `Bearer ${user.token}`,
-        },
-      };
-      const { data } = await axios.post(`/api/chat`, { userId }, config);
-
-      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
-      setSelectedChat(data);
-      setLoadingChat(false);
-      onClose();
-    } catch (error) {
-      toast({
-        title: "Error fetching the chat",
-        description: error.message,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-        position: "bottom-left",
-      });
-    }
-  };
-
   return (
     <>
       <Box
@@ -138,43 +92,43 @@ function SideDrawer() {
         <Text fontSize="2xl" fontFamily="Work sans">
           CulturaLang
         </Text>
-        {/* Add Home, Information, and Contact links */}
-        <Link href="/Home.html" ml={4} color="white">Home</Link>
-        <Link href="/Information.html" ml={4} color="white">Information</Link>
-        <Link href="/frontend\src\App.js" ml={4} color="white">Chat</Link>
-        <Link href="/Contact.htm" ml={4} color="white">Contact</Link>
+        <Link href="/Home.html" ml={4} color="white">
+          Home
+        </Link>
+        <Link href="/Information.html" ml={4} color="white">
+          Information
+        </Link>
+        <Link href="/frontend\src\App.js" ml={4} color="white">
+          Chat
+        </Link>
+        <Link href="/Contact.htm" ml={4} color="white">
+          Contact
+        </Link>
         <Button variant="ghost" onClick={onOpen}>
           <i className="fas fa-search"></i>
           <Text d={{ base: "none", md: "flex" }} px={4}>
             Search User
           </Text>
         </Button>
-        {/* Profile Menu */}
         <div>
           <Menu>
-            {/* Profile Button */}
-            <MenuButton as={Button} bg="white" rightIcon={<ChevronDownIcon />}>
-              <Avatar
-                size="sm"
-                cursor="pointer"
-                name={user.name}
-                src={user.pic}
-              />
+            <MenuButton
+              as={Button}
+              bg="white"
+              rightIcon={<ChevronDownIcon />}
+            >
+              <Avatar size="sm" cursor="pointer" name={user.name} src={user.pic} />
             </MenuButton>
-            {/* Profile Menu List */}
             <MenuList>
-              <ProfileModal user={user}>
-                {/* Apply custom style to "My Profile" item */}
-                <MenuItem color="black">My Profile</MenuItem>
-              </ProfileModal>
+              <MenuItem color="black">My Profile</MenuItem>
               <MenuDivider />
-              {/* Apply custom style to "Logout" item */}
-              <MenuItem color="black" onClick={logoutHandler}>Logout</MenuItem>
+              <MenuItem color="black" onClick={logoutHandler}>
+                Logout
+              </MenuItem>
             </MenuList>
           </Menu>
         </div>
       </Box>
-      {/* Drawer Content */}
       <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
         <DrawerContent>
@@ -190,9 +144,9 @@ function SideDrawer() {
               <Button onClick={handleSearch}>Go</Button>
             </Box>
             {loading ? (
-              <ChatLoading />
+              <Spinner />
             ) : (
-              searchResult?.map((user) => (
+              searchResult.map((user) => (
                 <UserListItem
                   key={user._id}
                   user={user}
@@ -200,7 +154,6 @@ function SideDrawer() {
                 />
               ))
             )}
-            {loadingChat && <Spinner ml="auto" d="flex" />}
           </DrawerBody>
         </DrawerContent>
       </Drawer>
@@ -209,4 +162,3 @@ function SideDrawer() {
 }
 
 export default SideDrawer;
-
