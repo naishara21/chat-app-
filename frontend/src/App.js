6086b245ffddd1db7affc176a0f17e832126de6e
Naisha <86354320+naishara21@users.noreply.github.com>
2024-03-31 00:25:51 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
-import React from 'react';
-import './App.css';
-import Homepage from './Pages/Homepage';
-import Chatpage from './Pages/Chatpage';
+import "./App.css";
+import Homepage from "./Pages/Homepage";
+import { Route } from "react-router-dom";
+import Chatpage from "./Pages/Chatpage";
 import Home from './Home.js';
-import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* Define routes using Switch */}
-      <Switch>
-        {/* Route for the Home component */}
-        <Route path="/home" component={Home} exact />
-        {/* Route for the Homepage component */}
-        <Route path="/homepage" component={Homepage} />
-        {/* Route for the Chatpage component */}
-        <Route path="/chats" component={Chatpage} />
-      </Switch>
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={Chatpage} />
+      <Route path="/home" component={Home} exact />
+
     </div>
   );
 }
