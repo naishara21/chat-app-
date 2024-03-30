455e0c99f68fa1a81f5007c6b4d36179896edc64
Naisha <86354320+naishara21@users.noreply.github.com>
2024-03-30 23:24:32 +0530

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,53 +1,53 @@
 import React from 'react';
 import './Home.css'; // Import CSS file
 
-// React component
 function Home() {
   return (
-    <Container>
-      <Nav>
-        <Title href="#">CULTURA LANG</Title>
-        <Menu>
-          <MenuItem>
+    <div className="Container">
+      <nav className="Nav">
+        <a className="Title" href="#">CULTURA LANG</a>
+        <ul className="Menu">
+          <li className="MenuItem">
             <a href="#">Home</a>
-          </MenuItem>
-          <MenuItem>
-            <Dropdown>
-              <DropdownToggle>Chat</DropdownToggle>
-              <DropdownContent>
+          </li>
+          <li className="MenuItem">
+            <div className="Dropdown">
+              <a className="DropdownToggle">Chat</a>
+              <div className="DropdownContent">
                 <a href="#">Link 1</a>
                 <a href="#">Link 2</a>
                 {/* Add more dropdown links as needed */}
-              </DropdownContent>
-            </Dropdown>
-          </MenuItem>
-          <MenuItem>
+              </div>
+            </div>
+          </li>
+          <li className="MenuItem">
             <a href="#">Information</a>
-          </MenuItem>
-          <MenuItem>
-            <Dropdown>
-              <DropdownToggle>Contact</DropdownToggle>
-              <DropdownContent>
+          </li>
+          <li className="MenuItem">
+            <div className="Dropdown">
+              <a className="DropdownToggle">Contact</a>
+              <div className="DropdownContent">
                 <a href="#">Link 1</a>
                 <a href="#">Link 2</a>
                 {/* Add more dropdown links as needed */}
-              </DropdownContent>
-            </Dropdown>
-          </MenuItem>
-        </Menu>
-      </Nav>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </nav>
       <img src="logo.png" alt="logo" />
-      <AboutContainer>
-        <AboutColumn>
-          <AboutTitle>About Us: Cultura Lang - Empowering Communities</AboutTitle>
-          <AboutParagraph>
+      <div className="AboutContainer">
+        <div className="AboutColumn">
+          <h3 className="AboutTitle">About Us: Cultura Lang - Empowering Communities</h3>
+          <p className="AboutParagraph">
             Welcome to Cultura Lang, a platform dedicated to fostering connections among students in grades 6th to 10th who share common interests and passions...
-          </AboutParagraph>
-        </AboutColumn>
+          </p>
+        </div>
         {/* Add more AboutColumns as needed */}
-      </AboutContainer>
-    </Container>
+      </div>
+    </div>
   );
 }
 
 export default Home;
+
